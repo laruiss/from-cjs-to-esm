89eddd8d1acabe9327447f9e4a4230195e97d991
Stanislas Ormières <stan@stormier.ninja>
2024-01-30 14:24:31 +0100

    chore: ✨ add client app and use shared

---
+++ apps/client/src/views/AppHome.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { add } from '@cjs-to-esm/shared'
+
+const result = add(1, 2)
+</script>
+
+<template>
+  <h1>Accueil</h1>
+
+  <p>
+    {{ result }}
+  </p>
+
+  <p>
+    <span class="fr-icon-ancient-gate-fill"></span> <!-- Exemple icône DSFR -->
+  </p>
+
+  <p>
+    <VIcon name="ri-flag-line" />  <!-- Exemple icône oh-vue-icon -->
+  </p>
+</template>
