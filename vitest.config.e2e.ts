9aece03ec939e48be378b4179f67b8d2cd9facff
Stanislas Ormières <stan@stormier.ninja>
2024-01-30 10:10:13 +0100

    feat: ✨ add nestjs and vitest, emit ESM

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,17 @@
+
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+  },
+  resolve: {
+    alias: {
+      '@/': './src/',
+    },
+  },
+  plugins: [swc.vite()],
+});
