276513dd5aa70c0404da7d3ba7250d2869a03cc3
Stanislas Ormières <stan@stormier.ninja>
2024-01-30 13:55:16 +0100

    feat: ✨ add vite config to use it with nestjs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module.js';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+if (import.meta.env.PROD) {
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  }
+
+  bootstrap();
 }
-bootstrap();
+
+export const viteNodeApp = NestFactory.create(AppModule);
